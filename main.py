997f18f43da23cdc8644a41857f1a5e3b4a1cb29
Jiege Chen <jiegec@qq.com>
2017-10-16 12:32:08 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/echo', methods=['POST'])
+def echo():
+    return 'Hello World and hello %s' % request.form['name']
+
+
+if __name__ == '__main__':
+    app.run()
